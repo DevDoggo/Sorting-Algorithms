1c260983cacfb6072de5e471b8f8bad0869df66b
DevDoggo <devdoggo@protonmail.com>
2017-11-22 15:49:54 +0100

    Added arguments and special-case handling listRandomizer.py + Finished insertion_sort.py Added the insertion-sort algorithm in insertion_sort.py Added running-functionality in insertion_sort.py for easy testing.

--- insertion_sort.py
+++ insertion_sort.py
@@ -3,4 +3,38 @@
 
 
 from listRandomizer import listRandomizer
-d = listRandomizer()
+lR = listRandomizer()
+
+class InsertionSorter():
+    def __init__(self):
+        pass
+        
+    def sortList(self, inputList):
+        if len(inputList) <= 1:
+            return inputList
+       
+        for j in range(1, len(inputList)):
+            key = inputList[j] 
+            i = j-1
+            while key < inputList[i] and i >= 0:
+                inputList[i+1] = inputList[i]
+                i = i-1
+            inputList[i + 1] = key
+        return inputList
+
+
+
+printout = ""
+while True:
+    x = input("Input Array Size [int]: ")
+    try:
+        newList = lR.getList(int(x))
+        insSorter = InsertionSorter()
+        printout = str(insSorter.sortList(newList))
+        break
+
+    except:
+        print("The input given is invalid.")
+        continue
+    
+print("\n" + printout + "\n")
--- listRandomizer.py
+++ listRandomizer.py
@@ -5,10 +5,13 @@ import random
 class listRandomizer():
     randomList = []
     def __init__(self):
-        print("List-Randomizer has been created.")
+        print("\nList-Randomizer has been created.")
 
-    def getList(self, listSize):
+    def getList(self, listSize, lowerBound = 0, upperBound = 100000):
+        if listSize < 1:
+            print("\nInvalid input!\n")
+            return
         for i in range(0, listSize):
-            self.randomList.append(random.randint(0, listSize))
+            self.randomList.append(random.randint(lowerBound, upperBound))
         return self.randomList
 
